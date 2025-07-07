21ffcdb7e70964519897227e67220e47b8a3027a
Khikmet <sadigzade.hi@mail.ru>
2025-07-07 09:05:12 +0300

    -

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,18 +1,18 @@
 import styles from "./index.module.css"
 
 const App = () => {
-  const handleRedirect = () => {
-    window.location.href =
-      "https://docs.google.com/document/d/1VbRGsf-39gTTFWbApv9aCJb9SkDoAN5zqkBS_O5Q-ps/edit?usp=sharing"
-  }
-
   return (
     <div className={styles.container}>
-      <button
-        className={styles.button}
-        onClick={handleRedirect}>
-        ТЫ ЧОРТ
-      </button>
+      <a
+        className={styles.link}
+        href="https://docs.google.com/document/d/1VbRGsf-39gTTFWbApv9aCJb9SkDoAN5zqkBS_O5Q-ps/edit?usp=sharing">
+        Goolge
+      </a>
+      <a
+        className={styles.link}
+        href="https://disk.yandex.ru/edit/d/hKGlMpiwG16w24Gac7zP_iPegnqahzm72s0qoIz-cKg6TndIN01BMzcwQQ">
+        Yandex
+      </a>
     </div>
   )
 }
